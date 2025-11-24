95c82c1deb6fd8b9ac2db420208f0dd8e1dde06e
it-center-program <it-center@nova-organic.com>
2025-11-24 17:23:45 +0700

    add log api fetch

--- src/services/fetchContact.js
+++ src/services/fetchContact.js
@@ -4,6 +4,7 @@ const API_URL = process.env.API_URL;
 
 export async function fetchContact(lastId = 0, limit = 1000) {
   const url = `${API_URL}/api/contactpoint?lastId=${lastId}&limit=${limit}`;
+  console.log(url);
   const res = await axios.get(url);
   return res.data; // assume array of records
 }
